81301535acedb0358a5520000042779f1e79e570
Tim Meusel <tim@bastelfreak.de>
2022-06-22 17:24:41 +0200

    Add supported_releases to get all non-EoL OS versions

--- spec/operatingsystem_spec.rb
+++ spec/operatingsystem_spec.rb
@@ -14,4 +14,12 @@ describe PuppetMetadata::OperatingSystem do
       expect(described_class.latest_release('Ubuntu')).to eq('22.04')
     end
   end
+  describe 'supported_releases' do
+    it 'returns 7 and 8 for CentOS' do
+      expect(described_class.supported_releases('CentOS')).to match_array(['7', '8'])
+    end
+    it 'returns 18.04, 20.04, 21.10 and 22.04 for Ubuntu' do
+      expect(described_class.supported_releases('Ubuntu')).to match_array(['18.04', '20.04', '21.10', '22.04'])
+    end
+  end
 end
