voxpupuli/puppet_metadata

commit 81301535acedb0358a5520000042779f1e79e570

Add supported_releases to get all non-EoL OS versions

lib/puppet_metadata/operatingsystem.rb
@@ -161,6 +161,15 @@ module PuppetMetadata
         releases = EOL_DATES[operatingsystem]
         releases&.keys&.max_by { |release| Gem::Version.new(release) }
       end
+
+      # Return an array of all Operating System versions that aren't EoL
+      # @param String operatingsystem The operating system
+      # @return [Array] All Operating System versions that aren't EoL today
+      def supported_releases(operatingsystem)
+        releases = EOL_DATES[operatingsystem]
+        today = Date.today
+        releases.compact.select {|release, eol_date| Date.parse(eol_date) > today}.keys.sort
+      end
     end
   end
 end

spec/operatingsystem_spec.rb
@@ -14,4 +14,12 @@ describe PuppetMetadata::OperatingSystem do
       expect(described_class.latest_release('Ubuntu')).to eq('22.04')
     end
   end
+  describe 'supported_releases' do
+    it 'returns 7 and 8 for CentOS' do
+      expect(described_class.supported_releases('CentOS')).to match_array(['7', '8'])
+    end
+    it 'returns 18.04, 20.04, 21.10 and 22.04 for Ubuntu' do
+      expect(described_class.supported_releases('Ubuntu')).to match_array(['18.04', '20.04', '21.10', '22.04'])
+    end
+  end
 end

commit cd6f839bc54b1f4d10c7606de874ee55420441af

Correct yard documentation tag
Data types need to be within braces to be recognized. Otherwise it's
interpreted as the parameter name String.

Fixes: 81301535acedb0358a5520000042779f1e79e570

lib/puppet_metadata/operatingsystem.rb
@@ -188,7 +188,7 @@ module PuppetMetadata
       end
 
       # Return an array of all Operating System versions that aren't EoL
-      # @param String operatingsystem The operating system
+      # @param [String] operatingsystem The operating system
       # @return [Array] All Operating System versions that aren't EoL today
       def supported_releases(operatingsystem)
         releases = EOL_DATES[operatingsystem]