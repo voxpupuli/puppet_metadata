voxpupuli/puppet_metadata

commit f0950e23c0c56056563b0a491d0175a1f1675ff8

supported_releases: treat OSes with `nil` as EoL date as non-EoL

lib/puppet_metadata/operatingsystem.rb
@@ -181,7 +181,8 @@ module PuppetMetadata
       def supported_releases(operatingsystem)
         releases = EOL_DATES[operatingsystem]
         today = Date.today
-        releases.compact.select {|release, eol_date| Date.parse(eol_date) > today}.keys.sort
+        tomorrow = (today + 1).to_s # hack to treat OS releases without EoL date as valid
+        releases.select {|release, eol_date| Date.parse(eol_date || tomorrow) > today}.keys.sort
       end
     end
   end

spec/operatingsystem_spec.rb
@@ -15,11 +15,14 @@ describe PuppetMetadata::OperatingSystem do
     end
   end
   describe 'supported_releases' do
-    it 'returns 7 and 8 for CentOS' do
-      expect(described_class.supported_releases('CentOS')).to match_array(['7', '8'])
+    it 'returns 7, 8 and 9 for CentOS' do
+      expect(described_class.supported_releases('CentOS')).to match_array(['7', '8', '9'])
     end
     it 'returns 18.04, 20.04, 21.10 and 22.04 for Ubuntu' do
       expect(described_class.supported_releases('Ubuntu')).to match_array(['18.04', '20.04', '21.10', '22.04'])
+    end
+    it 'returns 10 and 11 for Debian' do
+      expect(described_class.supported_releases('Debian')).to match_array(['10', '11'])
     end
   end
 end

commit 02c5fa31a671e1fc3fa3f9ede78637200a899215

Better deal with nil EOL dates
This avoids the need for an additional variable.

Fixes: f0950e23c0c56056563b0a491d0175a1f1675ff8

lib/puppet_metadata/operatingsystem.rb
@@ -193,8 +193,7 @@ module PuppetMetadata
       def supported_releases(operatingsystem)
         releases = EOL_DATES[operatingsystem]
         today = Date.today
-        tomorrow = (today + 1).to_s # hack to treat OS releases without EoL date as valid
-        releases.select {|release, eol_date| Date.parse(eol_date || tomorrow) > today}.keys.
+        releases.select { |release, eol_date| !eol_date || Date.parse(eol_date) > today }.keys.
           sort_by { |release| Gem::Version.new(release) }
       end
     end